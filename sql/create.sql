ed6715d1f07672d9250131e95f6e83d2e21bf440
Arthur <arthurdurand3@gmail.com>
2023-11-13 18:10:47 +0100

    finito les entitées fonctionnent

--- sql/create.sql
+++ sql/create.sql
@@ -3,21 +3,46 @@ CREATE TABLE IF NOT EXISTS etudiants (
     nom CHAR(32) UNIQUE NOT NULL,
     prenom CHAR(32) UNIQUE NOT NULL,
     mail CHAR(64) NOT NULL,
-    note FLOAT
+    mot_de_pass CHAR(64) NOT NULL,
+    date_de_naissance TIMESTAMP NOT NULL
 );
-
 
 CREATE TABLE IF NOT EXISTS dates (
     id_date SMALLSERIAL PRIMARY KEY,
     dat TIMESTAMP UNIQUE NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS villes (
+    id_ville SMALLSERIAL PRIMARY KEY,
+    nom CHAR(32)
+);
+
+CREATE TABLE IF NOT EXISTS voitures (
+    id_voiture SMALLSERIAL PRIMARY KEY,
+    marque CHAR(32),
+    typ CHAR(32),
+    couleur CHAR(32),
+    etat CHAR(32),
+    divers CHAR(64),
+    id_etudiant SMALLSERIAL,
+    FOREIGN KEY (id_etudiant) REFERENCES etudiants(id_etudiant)
+);
 
 CREATE TABLE IF NOT EXISTS voyages (
     id_voyage SMALLSERIAL PRIMARY KEY,
     nombre_places SMALLINT NOT NULL,
     id_voiture SMALLSERIAL,
     FOREIGN key (id_voiture) REFERENCES voitures(id_voiture)
+);
+
+CREATE TABLE IF NOT EXISTS destinations (
+    id_destination SMALLSERIAL PRIMARY KEY,
+    id_date SMALLSERIAL,
+    FOREIGN KEY (id_date) REFERENCES dates(id_date),
+    id_ville SMALLSERIAL,
+    FOREIGN KEY (id_ville) REFERENCES villes(id_ville),
+    id_voyage SMALLSERIAL,
+    FOREIGN key (id_voyage) REFERENCES voyages(id_voyage)
 );
 
 CREATE TABLE IF NOT EXISTS avis (
@@ -31,39 +56,13 @@ CREATE TABLE IF NOT EXISTS avis (
 
 CREATE TABLE IF NOT EXISTS reservations (
     id_reservation SMALLSERIAL PRIMARY KEY,
+    confirmation SMALLINT CHECK (confirmation <= 1),
     id_voyage SMALLSERIAL,
     FOREIGN KEY (id_voyage) REFERENCES voyages(id_voyage),
     id_date SMALLSERIAL,
     FOREIGN KEY (id_date) REFERENCES dates(id_date),
     id_ville SMALLSERIAL,
     FOREIGN KEY (id_ville) REFERENCES villes(id_ville),
-    id_etudiant SMALLSERIAL,
-    FOREIGN KEY (id_etudiant) REFERENCES etudiants(id_etudiant)
-);
-
-
-CREATE TABLE IF NOT EXISTS villes (
-    id_ville SMALLSERIAL PRIMARY KEY,
-    nom CHAR(32)
-);
-
-CREATE TABLE IF NOT EXISTS destinations (
-    id_destination SMALLSERIAL PRIMARY KEY,
-    id_date SMALLSERIAL,
-    FOREIGN KEY (id_date) REFERENCES dates(id_date),
-    id_ville SMALLSERIAL,
-    FOREIGN KEY (id_ville) REFERENCES villes(id_ville),
-    id_voyage SMALLSERIAL,
-    FOREIGN key (id_voyage) REFERENCES voyages(id_voyage)
-);
-
-CREATE TABLE IF NOT EXISTS voitures (
-    id_voiture SMALLSERIAL PRIMARY KEY,
-    marque CHAR(32),
-    typ CHAR(32),
-    couleur CHAR(32),
-    etat CHAR(32),
-    divers CHAR(64),
     id_etudiant SMALLSERIAL,
     FOREIGN KEY (id_etudiant) REFERENCES etudiants(id_etudiant)
 );

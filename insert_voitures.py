cd1341a23bf5106147b05d3e6e70a0b58f523fdf
Pierson Louis <lpierson002@enseirb-matmeca.fr>
2023-11-20 17:03:30 +0100

    Merge

--- insert_voitures.py
+++ insert_voitures.py
@@ -1,4 +1,5 @@
 import pandas as pd
+
 raw_data = pd.read_csv('./donnees/voitures.csv', sep=';')
 
 #open a file to write the sql commands
@@ -6,11 +7,11 @@ f = open('./sql/insert.sql', 'a')
 
 #append the sql commands to the file
 for i in range(len(raw_data)):
-    f.write("INSERT INTO voitures (marque, typ, couleur, nombre_places, etat, divers, id_etudiant) VALUES ('"+
+    f.write("INSERT INTO voitures (marque, modele, typ, couleur, etat, divers, id_etudiant) VALUES ('"+
             raw_data['marque'][i]+"','"+
+            raw_data['modele'][i]+"','"+
             raw_data['type'][i]+"','"+
             raw_data['couleur'][i]+"','"+
-            str(raw_data['nombre_places'][i])+"','"+
             raw_data['etat'][i]+"','"+
             raw_data['divers'][i]+"', "+
             str(i)+

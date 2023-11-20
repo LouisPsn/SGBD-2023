0161a1ccc0922d99e0f4b2ab68fb21b78bab348d
VERNANT Charlene <charlene.vernant@enseirb-matmeca.fr>
2023-11-20 17:36:30 +0100

    suppression dates et insert_voiture de trop

--- insert_voiture.py
+++
@@ -1,9 +0,0 @@
-
-voitures_csv_file = open("donnees/voitures.csv", "r")
-insert_sql_file = open("sql/insert.sql", "r")
-
-
-
-for line in voitures_csv_file:
-    line = line.split(";")
-    print(line)

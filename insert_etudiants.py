b2fe7c08c97d9f6fbd4ce23b8a3b5655c3f7cd0f
Pierson Louis <lpierson002@enseirb-matmeca.fr>
2023-11-20 17:56:27 +0100

    Debut de correction

--- insert_etudiants.py
+++ insert_etudiants.py
@@ -1,12 +1,18 @@
 import pandas as pd
+
 raw_data = pd.read_csv('./donnees/etudiants.csv', sep=';')
 
 #open a file to write the sql commands
-f = open('./sql/insert.sql', 'a')
+f = open('./sql/insert.sql', 'w')
 
 #append the sql commands to the file
 for i in range(len(raw_data)):
-    f.write("INSERT INTO etudiants (nom, prenom, mail, note) VALUES ('"+str(i)+raw_data['nom'][i]+"','"+raw_data['prenom'][i]+"','"+raw_data['mail'][i]+"');\n")
+    f.write("INSERT INTO etudiants (nom, prenom, mail, mot_de_passe, date_de_naissance) VALUES ('"=
+        raw_data['nom'][i]+"','"+
+        raw_data['prenom'][i]+"','"+
+        raw_data['mail'][i]+"','"+
+        raw_data['mot_de_passe'][i]+"','"+
+        raw_data['date_de_naissance'][i]");\n")
 
 #close the file
 f.close()

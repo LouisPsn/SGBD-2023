3025f3f18aa4e68706d196f515990401af3eb3da
VERNANT Charlene <charlene.vernant@enseirb-matmeca.fr>
2023-11-20 17:03:22 +0100

    correct insert_voitures

--- insert_voitures.py
+++ insert_voitures.py
@@ -8,12 +8,11 @@ f = open('./sql/insert.sql', 'a')
 for i in range(len(raw_data)):
     f.write("INSERT INTO voitures (marque, typ, couleur, nombre_places, etat, divers, id_etudiant) VALUES ('"+
             raw_data['marque'][i]+"','"+
+            raw_data['modele'][i]+"','"+
             raw_data['type'][i]+"','"+
             raw_data['couleur'][i]+"','"+
-            str(raw_data['nombre_places'][i])+"','"+
             raw_data['etat'][i]+"','"+
             raw_data['divers'][i]+"', "+
-            str(i)+
             ");\n")
 
 #close the file

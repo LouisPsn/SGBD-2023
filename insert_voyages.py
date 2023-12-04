7f9023712a95f380b719ff4dfec2e1fe4ac3979e
Pierson Louis <lpierson002@enseirb-matmeca.fr>
2023-12-04 17:19:28 +0100

    Ajout distance voyage

--- insert_voyages.py
+++ insert_voyages.py
@@ -11,9 +11,10 @@ conducteur = 2
 for i in range(len(raw_data)):
     f.write("INSERT INTO etapes (date, id_ville) VALUES ('2023-09-10', '"+str(i+1)+"');\n")
     f.write("INSERT INTO etapes (date, id_ville) VALUES ('2023-09-10', '"+str(i+2)+"');\n")
-    f.write("INSERT INTO voyages (nombre_places, id_voiture, etape_depart_voyage, etape_arrive_voyage) VALUES ("+
+    f.write("INSERT INTO voyages (nombre_places, id_voiture, distance ,etape_depart_voyage, etape_arrive_voyage) VALUES ("+
             str(raw_data['nombre_places'][i])+","+
             str(raw_data['id_voiture'][i])+","+
+            str(raw_data['distance'][i])+","+
             str(i+1)+","+
             str(i+2)+
             ");\n")

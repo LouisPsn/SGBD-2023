ab864480938a069d3980e0a42934aad5227bbfa0
Pierson Louis <lpierson002@enseirb-matmeca.fr>
2023-12-06 23:25:56 +0100

    Ajout de réservations fonctionnel

--- src/php/insert.php
+++ src/php/insert.php
@@ -1,5 +1,6 @@
 <?php
 $table = "$_POST[table]";
+$page = "../index.php";
 
 $params = parse_ini_file('../../database.ini');
 $dbconn = pg_connect("host=" . $params['host'] . " port=" . $params['port'] . " password=" . $params['password']);
@@ -12,7 +13,7 @@ if ($table === "etudiants") {
     $res = pg_query($dbconn, $query);
     $count = pg_fetch_row($res);
     if ($count[0] > 0) {
-        function_alert("Mauvais mot de passe");
+        function_alert("Mauvais mot de passe", $page);
     } else {
         $query = "INSERT INTO voitures (marque, modele, typ, couleur, etat, divers, id_etudiant) VALUES ('$_POST[marque]', '$_POST[modele]', '$_POST[typ]', '$_POST[couleur]', '$_POST[etat]', '$_POST[divers]', $_POST[id_etudiant]);";
     }
@@ -23,7 +24,7 @@ if ($table === "etudiants") {
     $res = pg_query($dbconn, $query);
     $count = pg_fetch_row($res);
     if ($count[0] == 0) {
-        function_alert("Mauvais mot de passe");
+        function_alert("Mauvais mot de passe", $page);
     } else {
         $query = "SELECT id_voiture FROM voitures JOIN etudiants ON etudiants.id_etudiant = voitures.id_etudiant WHERE etudiants.id_etudiant = $_POST[id_etudiant];";
         $result = pg_query($dbconn, $query);
@@ -50,20 +51,48 @@ if ($table === "etudiants") {
 
         $query = "INSERT INTO voyages (nombre_places, id_voiture, distance, etape_depart_voyage, etape_arrive_voyage) VALUES ($_POST[nombre_de_place], $id_voiture, $_POST[distance], $etape_depart_voyage, $etape_arrivee_voyage);";
     }
+} elseif ($table === "reservations") {
+    $page = "trajets";
+    $query = "SELECT COUNT(*) FROM etudiants WHERE id_etudiant = $_POST[id_etudiant] AND mot_de_passe = '$_POST[mot_de_passe]';";
+    $res = pg_query($dbconn, $query);
+    $count = pg_fetch_row($res);
+    if ($count[0] == 0) {
+        function_alert("Mauvais mot de passe", $page);
+    } else {
+        $date_depart = date('Y-m-d H:i:s', strtotime("$_POST[date_depart]"));
+        $query = "INSERT INTO etapes (date, id_ville) VALUES ('$date_depart', $_POST[id_ville_depart]);";
+        $result = pg_query($dbconn, $query);
+
+        $date_arrivee = date('Y-m-d H:i:s', strtotime("$_POST[date_arrivee]"));
+        $query = "INSERT INTO etapes (date, id_ville) VALUES ('$date_arrivee', $_POST[id_ville_arrivee]);";
+        $result = pg_query($dbconn, $query);
+
+        $query = "SELECT id_etape FROM etapes WHERE date = '$date_depart' AND id_ville = $_POST[id_ville_depart];";
+        $result = pg_query($dbconn, $query);
+        $result = pg_fetch_array($result);
+        $etape_depart = $result[0];
+
+        $query = "SELECT id_etape FROM etapes WHERE date = '$date_arrivee' AND id_ville = $_POST[id_ville_arrivee];";
+        $result = pg_query($dbconn, $query);
+        $result = pg_fetch_array($result);
+        $etape_arrive = $result[0];
+
+        $query = "INSERT INTO reservations (confirmation_reservation, id_voyage, date, proposition_prix, etape_depart_resa, etape_arrive_resa, id_etudiant) VALUES ('attente', $_POST[id_voyage], '2023-09-10', $_POST[prix], $etape_depart, $etape_arrive, $_POST[id_etudiant]);";
+    }
 }
 
 $res = pg_query($dbconn, $query);
 
-function function_alert($msg)
+function function_alert($msg, $table)
 {
     echo "<script type='text/javascript'>alert('$msg')
-        window.location.replace('../index.php');</script>";
+        window.location.replace('$table');</script>";
 }
 
 
 if ($res) {
-    function_alert("Les données ont été ajoutées avec succès");
+    function_alert("Les données ont été ajoutées avec succès", $page);
 } else {
-    function_alert("Vous avez dû rentrer de mauvaises informations");
+    function_alert("Vous avez dû rentrer de mauvaises informations", $page);
 }
 ?>
--- src/php/trajets.php
+++ src/php/trajets.php
@@ -29,7 +29,7 @@
 
   $params = parse_ini_file('../../database.ini');
 
-  $db_handle = pg_connect("host=".$params['host']." port=".$params['port']." password=".$params['password']);
+  $db_handle = pg_connect("host=" . $params['host'] . " port=" . $params['port'] . " password=" . $params['password']);
 
   // $sql_voyages = "SELECT * FROM voyages;";
   $sql_voyages =
@@ -44,7 +44,7 @@
      JOIN (SELECT id_etape, etapes.date, nom FROM etapes JOIN villes ON villes.id_ville = etapes.id_ville) as etapes2 ON etapes2.id_etape=voy_dep.etape_arrive_voyage
       ;"; */
 
-    "SELECT id_voyage , etudiants.nom, prenom , modele, couleur , etapes1.nom, etapes1.date, /* etape_arrive_voyage */  etapes2.nom, etapes2.date, etudiants.id_etudiant FROM voyages 
+    "SELECT id_voyage , etudiants.nom, prenom , modele, couleur , etapes1.nom, etapes1.date, /* etape_arrive_voyage */  etapes2.nom, etapes2.date, etudiants.id_etudiant, nombre_places FROM voyages 
 JOIN voitures ON voyages.id_voiture=voitures.id_voiture 
 JOIN etudiants ON voitures.id_etudiant = etudiants.id_etudiant 
 JOIN (SELECT id_etape, etapes.date, nom FROM etapes JOIN villes ON villes.id_ville = etapes.id_ville) as etapes1 ON etapes1.id_etape=voyages.etape_depart_voyage 
@@ -83,13 +83,14 @@ JOIN (SELECT id_etape, etapes.date, nom FROM etapes JOIN villes ON villes.id_vil
           <!-- <tbody> -->
           <?php
 
-          function extract_date($date) {
-            $ret = "le ".$date[5].$date[6]."/".$date[8].$date[9]."/".$date[0].$date[1].$date[2].$date[3]." à ".$date[10].$date[11]."h".$date[14].$date[15];
+          function extract_date($date)
+          {
+            $ret = "le " . $date[5] . $date[6] . "/" . $date[8] . $date[9] . "/" . $date[0] . $date[1] . $date[2] . $date[3] . " à " . $date[10] . $date[11] . "h" . $date[14] . $date[15];
             return $ret;
           }
 
           // echo "<tr> <td> Avant </td> </tr>";
-          while($row_voyage = pg_fetch_array($result_voyages)) {
+          while ($row_voyage = pg_fetch_array($result_voyages)) {
 
             // echo "<tr>";
             // echo "<th scope=\"row\">" . $row_voyage[0] . "</th>";
@@ -100,19 +101,19 @@ JOIN (SELECT id_etape, etapes.date, nom FROM etapes JOIN villes ON villes.id_vil
             // echo "</tr>";
             // echo "<tr>";
             // echo "<div class='voyage-complet'>";
-            echo "<tr class='ligne_voyage' id='voyage".$row_voyage[0]."'>";
+            echo "<tr class='ligne_voyage' id='voyage" . $row_voyage[0] . "'>";
 
-            echo "<td>".$row_voyage[0]."</td>";
-            echo "<td>".$row_voyage[1] . $row_voyage[2]."</td>";
-            echo "<td>".$row_voyage[3].$row_voyage[4]."</td>";
-            echo "<td>".$row_voyage[5].$row_voyage[6]."</td>";
-            echo "<td>".$row_voyage[7].$row_voyage[8]."</td>";
+            echo "<td>" . $row_voyage[0] . "</td>";
+            echo "<td>" . $row_voyage[1] . $row_voyage[2] . "</td>";
+            echo "<td>" . $row_voyage[3] . $row_voyage[4] . "</td>";
+            echo "<td>" . $row_voyage[5] . $row_voyage[6] . "</td>";
+            echo "<td>" . $row_voyage[7] . $row_voyage[8] . "</td>";
             echo "<td> </td>";
-            echo "<td> <button class='bouton_resa' id='resa_voyage".$row_voyage[0]."'>Show Resa</button> </td>";
+            echo "<td> <button class='bouton_resa' id='resa_voyage" . $row_voyage[0] . "'>Show Resa</button> </td>";
 
             echo "
-                              <form id='form-suppresion-voyage".$row_voyage[0]."' class='d-none' action='delete.php' method='post'>  
-                              <input type='hidden' name='id' value='form-suppresion-voyage".$row_voyage[0]."'>  
+                              <form id='form-suppresion-voyage" . $row_voyage[0] . "' class='d-none' action='delete.php' method='post'>  
+                              <input type='hidden' name='id' value='form-suppresion-voyage" . $row_voyage[0] . "'>  
                               <input type='hidden' name='page' value='trajets'>
                                 <input type='hidden' name='table' value='voyages'>
                                 <input type='hidden' name='id_voyage' value=$row_voyage[0]>
@@ -158,7 +159,7 @@ JOIN (SELECT id_etape, etapes.date, nom FROM etapes JOIN villes ON villes.id_vil
             */
             // echo "<tr> <td> Après </td> </tr>";
           
-            echo "<tr class='reservation-header resa resa_voyage".$row_voyage[0]."'><td> </td><td>Reservation</td><td>Passager</td><td>Etape départ</td><td>Etape arrivée</td><td>Prix proposé</td><td>Status</td><td>Suppression</td></tr>";
+            echo "<tr class='reservation-header resa resa_voyage" . $row_voyage[0] . "'><td> </td><td>Reservation</td><td>Passager</td><td>Etape départ</td><td>Etape arrivée</td><td>Prix proposé</td><td>Status</td><td>Suppression</td></tr>";
 
 
             $query_resa_du_voyage = "SELECT id_reservation, prenom, etape1.nom, etape2.nom, proposition_prix, confirmation_reservation, resa.id_etudiant FROM (SELECT id_reservation , prenom, etape_depart_resa, etape_arrive_resa, proposition_prix, confirmation_reservation, etudiants.id_etudiant FROM reservations
@@ -171,10 +172,10 @@ JOIN (SELECT id_etape, etapes.date, nom FROM etapes JOIN villes ON villes.id_vil
               JOIN (SELECT id_etape, nom FROM etapes JOIN villes ON villes.id_ville = etapes.id_ville) as etape2 ON etape2.id_etape=resa.etape_arrive_resa
               ;";
             $res_resa_du_voyage = pg_query($db_handle, $query_resa_du_voyage);
-            while($row_resa = pg_fetch_array($res_resa_du_voyage)) {
+            while ($row_resa = pg_fetch_array($res_resa_du_voyage)) {
 
               //   // function_alert($row[1]);
-              echo "<tr class ='resa resa_voyage".$row_voyage[0]."'><td> </td>";
+              echo "<tr class ='resa resa_voyage" . $row_voyage[0] . "'><td> </td>";
 
               //   // echo "<th scope=\"row\">" . $row_resa[0] . "</th>";
               //   // echo "<td>" . $row_resa[10] . "</td>";
@@ -182,9 +183,9 @@ JOIN (SELECT id_etape, etapes.date, nom FROM etapes JOIN villes ON villes.id_vil
               //   // echo "<td>" . $row_resa[6] . "</td>";
               //   // echo "<td>" . $row_resa[2] . "</td>";
           
-              for($i = 0; $i < 5; $i++) {
+              for ($i = 0; $i < 5; $i++) {
                 # code...
-                echo "<td>".$row_resa[$i]."</td>";
+                echo "<td>" . $row_resa[$i] . "</td>";
               }
               if ($row_resa[5] == "refuse") {
                 echo "<td>Refusé</td>";
@@ -196,52 +197,154 @@ JOIN (SELECT id_etape, etapes.date, nom FROM etapes JOIN villes ON villes.id_vil
 
 
               echo "
-                <form id='form-suppresion-resa".$row_resa[0]."' class='d-none' action='delete.php' method='post'>
-                  <input type='hidden' name='page' value='trajets'>
-                  <input type='hidden' name='table' value='reservations'>
-                  <input type='hidden' name='id_etudiant' value=$row_resa[6]>
-                  <input type='hidden' name='id_reservation' value=$row_resa[0]>
-                
-                  <!-- Button trigger modal -->
-                  <td>
-                  <center>
-                  <button type='button' class=' btn_smaller' data-bs-toggle='modal' data-bs-target='#passwordModalResa'>
-                    <span>X</span>
-                  </button>
-                  </center>
-                  </td>
-                
-                  <!-- Modal -->
-                  <div class='modal fade' id='passwordModalResa' tabindex='-1' aria-labelledby='exampleModalLabel' aria-hidden='true'>
-                    <div class='modal-dialog'>
-                      <div class='modal-content'>
-                        <div class='modal-header'>
-                          <h5 class='modal-title' id='exampleModalLabel'>Mot de Passe</h5>
-                          <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
+              <form id='form-suppresion-resa" . $row_resa[0] . "' class='d-none' action='delete.php' method='post'>
+                <input type='hidden' name='page' value='trajets'>
+                <input type='hidden' name='table' value='reservations'>
+                <input type='hidden' name='id_etudiant' value=$row_resa[6]>
+                <input type='hidden' name='id_reservation' value=$row_resa[0]>
+              
+                <!-- Button trigger modal -->
+                <td>
+                <center>
+                <button type='button' class=' btn_smaller' data-bs-toggle='modal' data-bs-target='#passwordModalResa'>
+                  <span>X</span>
+                </button>
+                </center>
+                </td>
+              
+                <!-- Modal -->
+                <div class='modal fade' id='passwordModalResa' tabindex='-1' aria-labelledby='exampleModalLabel' aria-hidden='true'>
+                  <div class='modal-dialog'>
+                    <div class='modal-content'>
+                      <div class='modal-header'>
+                        <h5 class='modal-title' id='exampleModalLabel'>Mot de Passe</h5>
+                        <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
+                      </div>
+                      <div class='modal-body'>
+                      <div class='input-group mb-3'>
+                        <input name='mot_de_passe' type='password' class='form-control' placeholder='Mot de Passe*'
+                          aria-label='Mot de Passe' aria-describedby='saisie-mot-de-passe'>
                         </div>
-                        <div class='modal-body'>
-                        <div class='input-group mb-3'>
-                          <input name='mot_de_passe' type='password' class='form-control' placeholder='Mot de Passe*'
-                            aria-label='Mot de Passe' aria-describedby='saisie-mot-de-passe'>
-                          </div>
-                        </div>
-                        <div class='modal-footer'>
-                          <button type='button' class='btn btn-secondary' data-bs-dismiss='modal'>Fermer</button>
-                          <button type='submit' class='btn btn-danger'>Envoyer</button>
-                        </div>
+                      </div>
+                      <div class='modal-footer'>
+                        <button type='button' class='btn btn-secondary' data-bs-dismiss='modal'>Fermer</button>
+                        <button type='submit' class='btn btn-danger'>Envoyer</button>
                       </div>
                     </div>
                   </div>
-                </form>";
-
-
+                </div>
+              </form>";
               echo "</tr>";
             }
             // echo "</div>";
             // echo "</tr>";
             // echo "<tr> </tr>";
+          
+            // Ajout réservations
+            $query = "SELECT COUNT(id_reservation) FROM reservations JOIN voyages ON voyages.id_voyage = reservations.id_voyage WHERE voyages.id_voyage = $row_voyage[0] AND  reservations.confirmation_reservation = 'accepte';";
+            $resultat = pg_query($db_handle, $query);
+            $count_resa = pg_fetch_array($resultat);
+
+            if ($row_voyage[10] - $count_resa[0] > 0) {
+              echo "
+              <form id='form-ajout-resa' class='d-none' action='insert.php' method='post'>
+                <input type='hidden' name='page' value='trajets'>
+                <input type='hidden' name='table' value='reservations'>
+                <input type='hidden' name='id_voyage' value=$row_voyage[0]>
+              
+                <!-- Button trigger modal -->
+                <td>
+                <center>
+                <button type='button' class='btn btn btn-dark' data-bs-toggle='modal' data-bs-target='#passwordModalResaAjj'>
+                  <span>Ajouter une réservation</span>
+                </button>
+                </center>
+                </td>
+              
+                <!-- Modal -->
+                <div class='modal fade' id='passwordModalResaAjj' tabindex='-1' aria-labelledby='exampleModalLabel' aria-hidden='true'>
+                  <div class='modal-dialog'>
+                    <div class='modal-content'>
+                      <div class='modal-header'>
+                        <h5 class='modal-title' id='exampleModalLabel'>Mot de Passe</h5>
+                        <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
+                      </div>
+                      <div class='modal-body'>
+
+                      <div class='input-group mb-3'>
+                        <select name='id_etudiant' class='form-select' aria-label='Default select example'>
+                          <option selected>Sélectionner un étudiant</option>
+                          ";
+                          $query = "SELECT * FROM etudiants LEFT OUTER JOIN voitures ON etudiants.id_etudiant = voitures.id_etudiant WHERE id_voiture is null ORDER BY nom;";
+                          $result = pg_query($db_handle, $query);
+                          while ($row = pg_fetch_array($result)) {
+                            echo "<option value=$row[0]>$row[1] $row[2]</option>";
+                          }
+                          echo "
+                        </select>
+                      </div>
+                        <div class='input-group mb-3'>
+                        <input name='prix' type='text' class='form-control' placeholder='Prix*'
+                          aria-label='Prix' aria-describedby='saisie-date-prix'>
+                        </div>
+                        <div class='input-group mb-3'>
+                          Étape de départ
+                        </div>
+                        <div class='input-group mb-3'>
+                          <input name='date_depart' type='datetime-local' class='form-control' placeholder='Date de départ*'
+                            aria-label='Date depart' aria-describedby='saisie-date-depart'>
+                        </div>
+                        <div class='input-group mb-3'>
+                          <select name='id_ville_depart' class='form-select' aria-label='Default select example'>
+                            <option selected>Sélectionner une ville de départ</option>
+                            ";
+                            $query = 'SELECT id_ville, nom FROM villes ORDER BY nom;';
+                            $result = pg_query($db_handle, $query);
+                            while ($row = pg_fetch_array($result)) {
+                              echo "<option value=$row[0]>$row[1]</option>";
+                            }
+                            echo "
+                          </select>
+                        </div>
+                        &nbsp;
+
+                        <div class='input-group mb-3'>
+                          Étape d'arrivée
+                        </div>
+                        <div class='input-group mb-3'>
+                          <input name='date_arrivee' type='datetime-local' class='form-control' placeholder='Date d'arrivée*''
+                            aria-label='Date arrivee' aria-describedby='saisie-date-arrivee'>
+                        </div>
+                        <div class='input-group mb-3'>
+                          <select name='id_ville_arrivee' class='form-select' aria-label='Default select example'>
+                            <option selected>Sélectionner une ville de d'arrivée'</option>
+                            ";
+                            $query = 'SELECT id_ville, nom FROM villes ORDER BY nom;';
+                            $result = pg_query($db_handle, $query);
+                            while ($row = pg_fetch_array($result)) {
+                              echo "<option value=$row[0]>$row[1]</option>";
+                            }
+                          echo "
+                          </select>
+                        </div>
+
+                        <div class='input-group mb-3'>
+                          <input name='mot_de_passe' type='password' class='form-control' placeholder='Mot de Passe*'
+                            aria-label='Mot de Passe' aria-describedby='saisie-mot-de-passe'>
+                        </div>
+                      </div>
+                      <div class='modal-footer'>
+                        <button type='button' class='btn btn-secondary' data-bs-dismiss='modal'>Fermer</button>
+                        <button type='submit' class='btn btn btn-dark'>Envoyer</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </form>";
+            }
           }
-          echo " ";
+
+
           ?>
           <!-- </tbody> -->
         </table>
